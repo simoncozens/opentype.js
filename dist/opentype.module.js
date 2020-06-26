0809daa862f82ed9cb4393479a38996331c60822
Simon Cozens <simon@simon-cozens.org>
2020-06-26 10:42:41 +0100

    Built version

--- dist/opentype.module.js
+++ dist/opentype.module.js
@@ -13617,6 +13617,65 @@ function parseFvarTable(data, start, names) {
 
 var fvar = { make: makeFvarTable, parse: parseFvarTable };
 
+// The `GDEF` table contains various glyph properties
+
+var attachList = function() {
+    return {
+        coverage: this.parsePointer(Parser.coverage),
+        attachPoints: this.parseList(Parser.pointer(Parser.uShortList))
+    }
+};
+
+var caretValue = function() {
+    var format = this.parseUShort();
+    check.argument(format == 1 || format == 2 || format == 3,
+        'Unsupported CaretValue table version.');
+    if (format == 1) {
+        return { coordinate: this.parseShort() }
+    } else if (format == 2) {
+        return { pointindex: this.parseShort() }
+    } else if (format == 3) {
+        // Device / Variation Index tables unsupported
+        return { coordinate: this.parseShort() }
+    }
+};
+
+var ligGlyph = function() {
+    return this.parseList(Parser.pointer(caretValue))
+};
+
+var ligCaretList = function() {
+    return {
+        coverage: this.parsePointer(Parser.coverage),
+        ligGlyphs: this.parseList(Parser.pointer(ligGlyph))
+    }
+};
+
+var markGlyphSets = function() {
+    this.parseUShort(); // Version
+    return this.parseList(Parser.pointer(Parser.coverage))
+};
+
+function parseGDEFTable(data, start) {
+    start = start || 0;
+    var p = new Parser(data, start);
+    var tableVersion = p.parseVersion(1);
+    check.argument(tableVersion == 1 || tableVersion == 1.2 || tableVersion == 1.3,
+        'Unsupported GDEF table version.');
+    var gdef = {
+        version: tableVersion,
+        classDef: p.parsePointer(Parser.classDef),
+        attachList: p.parsePointer(attachList),
+        ligCaretList: p.parsePointer(ligCaretList),
+        markAttachClassDef: p.parsePointer(Parser.classDef)
+    };
+    if (tableVersion >= 1.2) {
+        gdef.markGlyphSets = p.parsePointer(markGlyphSets);
+    }
+    return gdef
+}
+var gdef = { parse: parseGDEFTable };
+
 // The `GPOS` table contains kerning pairs, among other things.
 
 var subtableParsers$1 = new Array(10);         // subtableParsers[0] is unused
@@ -14014,6 +14073,7 @@ function parseBuffer(buffer, opt) {
     var cffTableEntry;
     var fvarTableEntry;
     var glyfTableEntry;
+    var gdefTableEntry;
     var gposTableEntry;
     var gsubTableEntry;
     var hmtxTableEntry;
@@ -14099,6 +14159,9 @@ function parseBuffer(buffer, opt) {
             case 'kern':
                 kernTableEntry = tableEntry;
                 break;
+            case 'GDEF':
+                gdefTableEntry = tableEntry;
+                break;
             case 'GPOS':
                 gposTableEntry = tableEntry;
                 break;
@@ -14137,6 +14200,11 @@ function parseBuffer(buffer, opt) {
         font.kerningPairs = kern.parse(kernTable.data, kernTable.offset);
     } else {
         font.kerningPairs = {};
+    }
+
+    if (gdefTableEntry) {
+        var gdefTable = uncompressTable(data, gdefTableEntry);
+        font.tables.gdef = gdef.parse(gdefTable.data, gdefTable.offset);
     }
 
     if (gposTableEntry) {
